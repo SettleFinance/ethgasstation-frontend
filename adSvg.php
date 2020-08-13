5656254880a7125e924df2b9f532822293f6c2a1
sajibcse68 <sajibcse68@gmail.com>
2020-08-14 01:22:58 +0600

    Insert Bloq ad 2

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
-    <a href="https://hack.chain.link/?utm_campaign=clhackathon&utm_medium=banner&utm_source=ethgasstaion" target="_blank">
-      <img class="ad-desktop" src="/images/chainlink-desktop.svg">
-      <img class="ad-mobile" src="/images/chainlink-mobile.svg">
+    <a href="https://www.bloq.com/egs/" target="_blank">
+      <img class="ad-desktop" src="/images/bloq-desktop.svg">
+      <img class="ad-mobile" src="/images/bloq-mobile.svg">
     </a>
   </div>
 </div>
