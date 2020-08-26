f9848435fdc353a1855fdaa5da59dbbdbcbbe260
sajibcse68 <sajibcse68@gmail.com>
2020-08-27 01:11:09 +0600

    Update chainlink-ad href link

--- adSvg.php
+++ adSvg.php
@@ -1,6 +1,6 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
-    <a href="https://www.bloq.com/egs/" target="_blank" href="https://www.bloq.com/egs/" target="_blank">
+    <a href="https://hack.chain.link/?utm_campaign=clhackathon&utm_medium=banner&utm_source=ethgasstaion" target="_blank">
       <img class="ad-desktop" src="/images/chainlink-desktop.svg">
       <img class="ad-mobile" src="/images/chainlink-mobile.svg">
     </a>
