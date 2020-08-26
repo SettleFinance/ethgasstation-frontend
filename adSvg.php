f5bc52dcaa503015c545e810308ce86ae1bb50ee
sajibcse68 <sajibcse68@gmail.com>
2020-08-27 00:41:04 +0600

    Ad chainlink ad

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
     <a href="https://www.bloq.com/egs/" target="_blank" href="https://www.bloq.com/egs/" target="_blank">
-      <img class="ad-desktop" src="/images/bloq-desktop.svg">
-      <img class="ad-mobile" src="/images/bloq-mobile.svg">
+      <img class="ad-desktop" src="/images/chainlink-desktop.svg">
+      <img class="ad-mobile" src="/images/chainlink-mobile.svg">
     </a>
   </div>
 </div>
