0f6ba125ff2c37a4a8a9db93b07e79eb23c11bb1
sajibcse68 <sajibcse68@gmail.com>
2020-09-17 23:29:45 +0600

    Change bloq ad to bloq-eth ad

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
-    <a href="https://hack.chain.link/?utm_campaign=clhackathon&utm_medium=banner&utm_source=ethgasstaion" target="_blank">
-      <img class="ad-desktop" src="/images/chainlink-desktop.svg">
-      <img class="ad-mobile" src="/images/chainlink-mobile.svg">
+    <a href="https://zoom.us/webinar/register/WN_x13OukJ_SvyUpgDyu44Fdg" target="_blank">
+      <img class="ad-desktop" src="/images/bloq-eth-desktop.svg">
+      <img class="ad-mobile" src="/images/bloq-eth-mobile.svg">
     </a>
   </div>
 </div>
