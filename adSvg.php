88f37407ace74d59a288d73a25d3734c60210186
sajibcse68 <sajibcse68@gmail.com>
2020-09-17 23:29:45 +0600

    Change bloq ad to bloq-eth ad

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
-    <a href="https://www.bloq.com/egs/" target="_blank">
-      <img class="ad-desktop" src="/images/bloq-desktop.svg">
-      <img class="ad-mobile" src="/images/bloq-mobile.svg">
+    <a href="https://zoom.us/webinar/register/WN_x13OukJ_SvyUpgDyu44Fdg" target="_blank">
+      <img class="ad-desktop" src="/images/bloq-eth-desktop.svg">
+      <img class="ad-mobile" src="/images/bloq-eth-mobile.svg">
     </a>
   </div>
 </div>
