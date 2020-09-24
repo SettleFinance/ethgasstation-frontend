938bc5e6ec31b51caecfb4c8c576661ed727ffb4
sajibcse68 <sajibcse68@gmail.com>
2020-09-24 22:00:53 +0600

    Update & bloq ad, remove bloq-eth ad

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
-    <a href="https://zoom.us/webinar/register/WN_x13OukJ_SvyUpgDyu44Fdg" target="_blank">
-      <img class="ad-desktop" src="/images/bloq-eth-desktop.svg">
-      <img class="ad-mobile" src="/images/bloq-eth-mobile.svg">
+    <a href="https://sync.bloq.com/how-to-build-defi-alerts-and-price-feeds-blockchain-api/" target="_blank">
+      <img class="ad-desktop" src="/images/bloq-desktop.svg">
+      <img class="ad-mobile" src="/images/bloq-mobile.svg">
     </a>
   </div>
 </div>
