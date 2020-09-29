619a01a3df0e3f5148b30f4172a4adb61c04dbe9
sajibcse68 <sajibcse68@gmail.com>
2020-09-30 00:11:22 +0600

    Update Bloq ad

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
-    <a href="https://sync.bloq.com/how-to-build-defi-alerts-and-price-feeds-blockchain-api/" target="_blank">
-      <img class="ad-desktop" src="/images/bloq-desktop.svg">
-      <img class="ad-mobile" src="/images/bloq-mobile.svg">
+    <a href="https://www.bloq.com/egs/" target="_blank">
+      <img class="ad-desktop" src="/images/bloq-desktop.svg" alt="Bloq Ad">
+      <img class="ad-mobile" src="/images/bloq-mobile.svg" alt="Bloq Ad">
     </a>
   </div>
 </div>
