737f2e410f45139531f394cf0321b88173356144
sajibcse68 <sajibcse68@gmail.com>
2020-10-17 01:23:00 +0600

    Update farmer ad

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
-    <a href="https://www.bloq.com/egs/" target="_blank">
-      <img class="ad-desktop" src="/images/farmer.png" alt="Farmer Ad">
-      <img class="ad-mobile" src="/images/farmer-mobile.png" alt="Farmer Ad">
+    <a href="https://yieldfarmer.substack.com/" target="_blank">
+      <img class="ad-desktop" src="/images/farmer-desktop.png" alt="Farmer Ad">
+      <!-- <img class="ad-mobile" src="/images/farmer-mobile.png" alt="Farmer Ad"> -->
     </a>
   </div>
 </div>
