be70cdbdc1bda689753e0f04e828e9890eafa417
sajibcse68 <sajib@concourseopen.com>
2020-11-12 00:12:03 +0600

    Integrate EGS first ad slot and serve ad by double click

---
+++ adByGoogle.php
@@ -0,0 +1,6 @@
+<!-- /22134506417/egs-1st-ad-unit -->
+<div id='div-gpt-ad-1605111586409-0'>
+  <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1605111586409-0'); });
+  </script>
+</div>
