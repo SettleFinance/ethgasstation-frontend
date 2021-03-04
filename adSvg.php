fbe7c5a7d42b649c9bfff70885b3835bc8601d9a
Brian Ford <brian@concourseopen.com>
2021-03-03 22:02:49 -0500

    rename css classes

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,8 @@
 <div class="ad-wrap">
   <div class="ad-svg-cont">
     <a href="http://bit.ly/3c1v8ds" target="_blank">
-      <img class="ad-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
-      <img class="ad-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
+      <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
+      <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
     </a>
   </div>
 </div>
