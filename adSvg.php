09f3ad9cee482ce9022b5f247aef277793eb1568
Brian Ford <brian@concourseopen.com>
2021-03-04 15:46:59 -0500

    chainlink ad

--- adSvg.php
+++ adSvg.php
@@ -1,8 +1,13 @@
 <div class="promo-wrap">
   <div class="promo-svg-cont">
-    <a href="http://bit.ly/3c1v8ds" target="_blank">
+    <!-- DEFAULT DFP FARMER NEWSLETTER -->
+    <!-- <a href="http://bit.ly/3c1v8ds" target="_blank">
       <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
       <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
+    </a> -->
+    <a href="https://chain.link/hackathon?utm_source=defi-pulse&utm_medium=banner-v1&utm_campaign=careers" target="_blank">
+      <img class="promo-desktop" src="/images/chainlink/v1_desktop.svg" alt="Chainlink Hiring Banner">
+      <img class="promo-mobile" src="/images/chainlink/v1_mobile.svg" alt="Chainlink Hiring Banner">
     </a>
   </div>
 </div>
