fb16fd3e42220d410bc2332930729489ca3acf42
Brian Ford <brian@concourseopen.com>
2021-03-08 10:12:32 -0500

    update banner

--- adSvg.php
+++ adSvg.php
@@ -5,7 +5,7 @@
       <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
       <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
     </a> -->
-    <a href="https://chain.link/hackathon?utm_source=defi-pulse&utm_medium=banner&utm_campaign=hackathon" target="_blank">
+    <a href="https://chain.link/hackathon?utm_source=eth-gas-station&utm_medium=banner&utm_campaign=hackathon" target="_blank">
       <img class="promo-desktop" src="/images/chainlink/hackathon_desktop.svg" alt="Chainlink Hackathon Banner">
       <img class="promo-mobile" src="/images/chainlink/hackathon_mobile.svg" alt="Chainlink Hackathon Banner">
     </a>
