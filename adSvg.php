8e2f3669c28fbabd6054b5a9e0747aab3c6fdabd
Brian Ford <brian@concourseopen.com>
2021-03-15 12:43:47 -0400

    update chainlink banner

--- adSvg.php
+++ adSvg.php
@@ -5,9 +5,9 @@
       <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
       <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
     </a> -->
-    <a href="https://chain.link/hackathon?utm_source=eth-gas-station&utm_medium=banner&utm_campaign=hackathon" target="_blank">
-      <img class="promo-desktop" src="/images/chainlink/hackathon_desktop.svg" alt="Chainlink Hackathon Banner">
-      <img class="promo-mobile" src="/images/chainlink/hackathon_mobile.svg" alt="Chainlink Hackathon Banner">
+    <a href="https://www.chainlinklabs.com/careers?utm_source=eth-gas-station&utm_medium=banner-v1&utm_campaign=careers" target="_blank">
+      <img class="promo-desktop" src="/images/chainlink/v2_desktop.svg" alt="Chainlink Hiring Banner">
+      <img class="promo-mobile" src="/images/chainlink/v2_mobile.svg" alt="Chainlink Hiring Banner">
     </a>
   </div>
 </div>
