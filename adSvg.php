2ce20dcda91b0596f3b8c6df1fa1fad054d7f89f
Brian Ford <brian@concourseopen.com>
2021-03-18 12:38:41 -0400

    update link

--- adSvg.php
+++ adSvg.php
@@ -5,7 +5,7 @@
       <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
       <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
     </a> -->
-    <a href="http://bit.ly/3eLYeAE" target="_blank">
+    <a href="http://bit.ly/3r3EeMi" target="_blank">
       <img class="promo-desktop" src="/images/golem/banner_desktop.svg" alt="Golem Banner">
       <img class="promo-mobile" src="/images/golem/banner_mobile.svg" alt="Golem Banner">
     </a>
