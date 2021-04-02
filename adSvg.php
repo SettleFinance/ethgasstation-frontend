2db6f94bbeb84ee6dd31bd2725f8b58bce8ae6f6
Brian Ford <brian@concourseopen.com>
2021-04-02 10:27:36 -0400

    deversifi ad

--- adSvg.php
+++ adSvg.php
@@ -5,9 +5,10 @@
       <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
       <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
     </a> -->
-    <a href="http://bit.ly/3r3EeMi" target="_blank">
-      <img class="promo-desktop" src="/images/golem/banner_desktop.svg" alt="Golem Banner">
-      <img class="promo-mobile" src="/images/golem/banner_mobile.svg" alt="Golem Banner">
+    <a class="promo-desktop" href="https://bit.ly/3sN42hh" target="_blank">
+      <img class="promo-desktop" src="/images/deversifi/banner_desktop.svg" alt="Deversifi Banner">
     </a>
+    <a class="promo-mobile" href="https://bit.ly/3rTlG1X" target="_blank">
+    <img class="promo-mobile" src="/images/deversifi/banner_mobile.svg" alt="Deversifi Banner">
   </div>
 </div>
