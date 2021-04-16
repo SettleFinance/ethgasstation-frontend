e1effad8f95684fad0ac104e7ac27e54998dd78d
Brian Ford <brian@concourseopen.com>
2021-04-16 14:08:38 -0400

    update banner ad

--- adSvg.php
+++ adSvg.php
@@ -5,10 +5,10 @@
       <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
       <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
     </a> -->
-    <a class="promo-desktop" href="https://bit.ly/3sN42hh" target="_blank">
-      <img class="promo-desktop" src="/images/deversifi/banner_desktop.svg" alt="Deversifi Banner">
+    <a class="promo-desktop" href="https://bit.ly/3r3EeMi" target="_blank">
+      <img class="promo-desktop" src="/images/golem/banner_desktop.svg" alt="Golem Banner">
     </a>
-    <a class="promo-mobile" href="https://bit.ly/3rTlG1X" target="_blank">
-    <img class="promo-mobile" src="/images/deversifi/banner_mobile.svg" alt="Deversifi Banner">
+    <a class="promo-mobile" href="https://bit.ly/3r3EeMi" target="_blank">
+    <img class="promo-mobile" src="/images/golem/banner_mobile.svg" alt="Golem Banner">
   </div>
 </div>
