88a78272d178e60f61bcb439e5ec67d0e42dd048
Brian Ford <brian@concourseopen.com>
2021-04-23 15:04:19 -0400

    test new multi ad feature basics

--- adSvg.php
+++ adSvg.php
@@ -5,11 +5,26 @@
       <img class="promo-desktop" src="/images/farmer_desktop_purple.svg" alt="DFP Farmer Newsletter">
       <img class="promo-mobile" src="/images/farmer_mobile_purple.svg" alt="DFP Farmer Newsletter">
     </a> -->
+    <?php
+
+      class ad {
+        public $href;
+        public $desktopImg;
+        public $mobileImg;
+        public $alt;
+      }
+
+      $golemAd = new ad();
+      $golemAd->href = 'https://bit.ly/3r3EeMi';
+
+      $ads = array($golemAd);
+    ?>
+
     <a class="promo-desktop" href="https://bit.ly/3r3EeMi" target="_blank">
       <img class="promo-desktop" src="/images/golem/banner_desktop.svg" alt="Golem Banner">
     </a>
-    <a class="promo-mobile" href="https://bit.ly/3r3EeMi" target="_blank">
-    <img class="promo-mobile" src="/images/golem/banner_mobile.svg" alt="Golem Banner">
+    <a class="promo-mobile" href="<?php echo ($ads[0]->href); ?>" target="_blank">
+    <img class="promo-mobile" src="/images/golem/banner_mobile.svg" alt="Golem Banner2">
     </a>
   </div>
 </div>
