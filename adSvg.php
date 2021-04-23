a9b4dba0be9788ec658f9a034144fcd881d2d919
Brian Ford <brian@concourseopen.com>
2021-04-23 15:22:43 -0400

    multi ad support

--- adSvg.php
+++ adSvg.php
@@ -16,15 +16,18 @@
 
       $golemAd = new ad();
       $golemAd->href = 'https://bit.ly/3r3EeMi';
+      $golemAd->desktopImg = '/images/golem/banner_desktop.svg';
+      $golemAd->mobileImg = '/images/golem/banner_mobile.svg';
+      $golemAd->alt = 'Golem Banner3';
 
       $ads = array($golemAd);
     ?>
 
-    <a class="promo-desktop" href="https://bit.ly/3r3EeMi" target="_blank">
-      <img class="promo-desktop" src="/images/golem/banner_desktop.svg" alt="Golem Banner">
+    <a class="promo-desktop" href="<?php echo ($ads[0]->href); ?>" target="_blank">
+      <img class="promo-desktop" src="<?php echo ($ads[0]->desktopImg); ?>" alt="<?php echo ($ads[0]->alt); ?>">
     </a>
     <a class="promo-mobile" href="<?php echo ($ads[0]->href); ?>" target="_blank">
-    <img class="promo-mobile" src="/images/golem/banner_mobile.svg" alt="Golem Banner2">
+    <img class="promo-mobile" src="<?php echo ($ads[0]->mobileImg); ?>" alt="<?php echo ($ads[0]->alt); ?>">
     </a>
   </div>
 </div>
